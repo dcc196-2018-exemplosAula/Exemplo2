470fed612773dd6ae95edbda8b746568423be4cd
YanPaiva <gckael@gmail.com>
2018-08-29 20:35:28 -0300

    Adciona notificações para o estado da professorACT

--- app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exemplo02/ProfessorActivity.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc196.exemplo02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfessorActivity extends AppCompatActivity {
 
@@ -12,12 +13,50 @@ public class ProfessorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
         txtProfMensagem = findViewById(R.id.txt_prof_mensagem);
-        Bundle extras  = getIntent().getExtras();
-        if(extras != null){
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
             String nome = extras.getString(MainActivity.pessoa_nome);
-            txtProfMensagem.setText("Olá prof. "+nome+"!");
+            txtProfMensagem.setText("Olá prof. " + nome + "!");
 
 
         }
+        //alerta curto
+        Toast.makeText(getApplicationContext(), "prof onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "prof onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "prof onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "prof onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "prof onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "prof onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "prof onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
